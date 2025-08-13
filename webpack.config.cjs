7cfc5d1d8327d0a6f72e93a121801e03ebfb0350
GeetaChahar <vgeetachahar@gmail.com>
2025-08-13 20:15:56 +0530

    build error fixed

---
+++ webpack.config.cjs
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "auth-token-lib.js",
+    library: "AUTH_TOKEN_LIB",
+    libraryTarget: "umd", // works in browser & Node
+    globalObject: "this", // fix for Node/browser compatibility
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
